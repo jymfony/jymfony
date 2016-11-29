461ee4291d8631f23e41f9d986164be7adf3dea8
Alessandro Chitolina <alekitto@gmail.com>
2016-11-29 12:08:34 +0100

    is module must be the last registered

--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,7 @@
 require('./lib/obj2map');
-require('./lib/is');
 require('./lib/Function/BoundFunction');
 require('./lib/Function/callableArray');
 require('./lib/Function/getFunction');
 require('./lib/String/strtr');
+
+require('./lib/is');
