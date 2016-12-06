0235e18547f6534481f27aae7284b9874552b8ad
Alessandro Chitolina <alekitto@gmail.com>
2016-12-06 22:24:44 +0100

    [Exceptions] added previous parameters to exception object

--- src/Exceptions/lib/Exception.js
+++ src/Exceptions/lib/Exception.js
@@ -1,8 +1,9 @@
 class Exception extends Error {
-    constructor(message) {
+    constructor(message, previous = undefined) {
         super(message);
 
         this.name = this.constructor.name;
+        this.previous = previous;
         this.message = message;
         if (typeof Error.captureStackTrace === 'function') {
             Error.captureStackTrace(this, this.constructor);
