9bc9c9953c7c770e16c744e0f801f7831cfad3c1
Alessandro Chitolina <alekitto@gmail.com>
2016-12-11 09:29:37 +0100

    defined interfaces as mixins (multiple extensions)

--- src/util/lib/interface.js
+++ src/util/lib/interface.js
@@ -1,9 +1,22 @@
-class Interface {
-    constructor(throwError = true) {
-        if (throwError) {
-            throw new Error('Cannot instantiate ' + this.constructor.name);
-        }
-    }
-}
+global.getInterface = function (superclass) {
+    let mixin = () => class extends superclass {};
+    Object.setPrototypeOf(mixin, superclass.prototype);
+    mixin[Symbol.hasInstance] = (o) => {
+        let mixins = o.constructor['_applied_mixins'];
+        return mixins.indexOf(mixin) != -1;
+    };
 
-global.Interface = Interface;
+    return mixin;
+};
+
+global.mix = function (superclass, ...interfaces) {
+    superclass = superclass || class {};
+    superclass = interfaces.reduce((a, b) => b(a), superclass);
+
+    let mixed = (s => class extends s {})(superclass);
+    Object.defineProperty(mixed, '_applied_mixins', {
+        value: [superclass, ...interfaces],
+        enumerable: false
+    });
+    return mixed;
+};
