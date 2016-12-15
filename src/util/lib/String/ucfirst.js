b8b9fae2ee10e05f199b945dcd72f81b1f07a0fa
Alessandro Chitolina <alekitto@gmail.com>
2016-12-15 22:06:57 +0100

    added ucfirst

---
+++ src/util/lib/String/ucfirst.js
@@ -0,0 +1,12 @@
+'use strict';
+
+global.__jymfony = global.__jymfony || {};
+
+/**
+ * Uppercase first character
+ *
+ * @param {string} string
+ */
+global.__jymfony.ucfirst = function (string) {
+    return string.charAt(0).toUpperCase() + string.substring(1);
+};
