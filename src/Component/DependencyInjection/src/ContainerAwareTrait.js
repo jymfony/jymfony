cff08252b531135279442167f051366e150c7ce9
Alessandro Chitolina <alekitto@gmail.com>
2016-12-16 00:16:53 +0100

    [DI] added ContainerAwareTrait

---
+++ src/Component/DependencyInjection/src/ContainerAwareTrait.js
@@ -0,0 +1,21 @@
+
+/**
+ * @memberOf Jymfony.DependencyInjection
+ * @type {Jymfony.DependencyInjection.ContainerAwareTrait}
+ */
+module.exports = getTrait(class ContainerAwareTrait {
+    /**
+     * Sets the container
+     *
+     * @param {Jymfony.DependencyInjection.Container} container
+     */
+    setContainer(container) {
+        /**
+         * Service container
+         *
+         * @type {Jymfony.DependencyInjection.Container}
+         * @protected
+         */
+        this._container = container;
+    }
+});
