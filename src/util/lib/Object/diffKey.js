c7c59b12c4f756fc7b3851a8aee15d8435b25b05
Alessandro Chitolina <alekitto@gmail.com>
2016-12-15 22:31:22 +0100

    added diff key util function

---
+++ src/util/lib/Object/diffKey.js
@@ -0,0 +1,26 @@
+global.__jymfony = global.__jymfony || {};
+
+let diffKey = function (arr1, ...arrays) {
+    let inArrays = key => {
+        for (let array of arrays) {
+            if (key in array) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    let retArr = {};
+    for (let key of Object.keys(arr1)) {
+        if (inArrays(key)) {
+            continue;
+        }
+
+        retArr[key] = arr1[key];
+    }
+
+    return retArr;
+};
+
+global.__jymfony.diff_key = diffKey;
