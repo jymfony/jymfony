dad2136764e21d8a167aacec6fe42734cfb4a6d1
Alessandro Chitolina <alekitto@gmail.com>
2016-12-17 20:12:31 +0100

    include diff_key and ucfirst in index

--- src/util/index.js
+++ src/util/index.js
@@ -6,11 +6,13 @@ require('./lib/is');
 require('./lib/Object/entries');
 require('./lib/Object/obj2map');
 require('./lib/Object/merge');
+require('./lib/Object/diffKey');
 
 require('./lib/Function/callableArray');
 require('./lib/Function/getFunction');
 
 require('./lib/String/strtr');
+require('./lib/String/ucfirst');
 
 require('./lib/mixins');
 
