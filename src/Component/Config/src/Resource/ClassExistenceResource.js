8b428191ce9ba58fff2a8db433d4fbcd2e090270
Alessandro Chitolina <alekitto@gmail.com>
2016-12-24 16:02:56 +0100

    [Config] added FileResource and ClassExistenceResource

---
+++ src/Component/Config/src/Resource/ClassExistenceResource.js
@@ -0,0 +1,24 @@
+const SelfCheckingResourceInterface = Jymfony.Config.Resource.SelfCheckingResourceInterface;
+
+module.exports = class ClassExistenceResource extends implementationOf(SelfCheckingResourceInterface) {
+    constructor(resource) {
+        super();
+
+        this._resource = resource;
+        this._exists = ReflectionClass.exists(resource);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    toString() {
+        return this._resource;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    isFresh(timestamp) {
+        return this._exists === ReflectionClass.exists(this._resource);
+    }
+};
---
+++ src/Component/Config/src/Resource/FileResource.js
@@ -0,0 +1,24 @@
+const SelfCheckingResourceInterface = Jymfony.Config.Resource.SelfCheckingResourceInterface;
+const fs = require('fs');
+
+module.exports = class FileResource extends implementationOf(SelfCheckingResourceInterface) {
+    constructor(resource) {
+        super();
+
+        this._resource = fs.realpathSync(resource);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    toString() {
+        return this._resource;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    isFresh(timestamp) {
+        return fs.existsSync(this._resource) && fs.statSync(this._resource).mtime <= timestamp;
+    }
+};
