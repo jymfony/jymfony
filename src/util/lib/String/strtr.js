618199ccfc9dd2f613c50e89f27d5592b2aa238c
Alessandro Chitolina <alekitto@gmail.com>
2017-10-04 21:32:56 +0200

    [util] add test and fixed strtr function

--- src/util/lib/String/strtr.js
+++ src/util/lib/String/strtr.js
@@ -3,13 +3,11 @@
 global.__jymfony = global.__jymfony || {};
 
 global.__jymfony.strtr = function strtr(string, replacePairs) {
-    let str = string.toString(), re;
+    const searchPattern = Object.keys(replacePairs)
+        .map(__jymfony.regex_quote)
+        .join('|');
 
-    for (let [ key, value ] of __jymfony.getEntries(replacePairs)) {
-        key = key.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
-        re = new RegExp(key, 'g');
-        str = str.replace(re, value);
-    }
-
-    return str;
+    return string.toString().replace(new RegExp(searchPattern, 'g'), (match) => {
+        return replacePairs[match];
+    });
 };
---
+++ src/util/test/String/StrtrTest.js
@@ -0,0 +1,16 @@
+require('../../lib/String/strtr');
+const expect = require('chai').expect;
+
+describe('Strtr', function () {
+    it('should work correctly', () => {
+        const trans = {'hello': 'hi', 'hi': 'hello', 'a': 'A', 'world': 'planet'}
+        expect(__jymfony.strtr("# hi all, I said hello world! #", trans))
+            .to.be.equal('# hello All, I sAid hi planet! #')
+    });
+
+    it('should work correctly with regex special chars', () => {
+        const trans = {'hello': 'hi', '\\hi?': 'hello#', '(a': ')A', 'world': 'planet'}
+        expect(__jymfony.strtr("# \\hi? (all), I said hello world! #", trans))
+            .to.be.equal('# hello# )All), I said hi planet! #')
+    });
+});
