c92ac992dd1ff0cb26f24948c784ff57ad3387a0
Alessandro Chitolina <alekitto@gmail.com>
2018-10-16 01:09:48 +0200

    [HttpFoundation] add sessions

---
+++ src/Component/HttpFoundation/src/Session/AttributeBagInterface.js
@@ -0,0 +1,60 @@
+const SessionBagInterface = Jymfony.Component.HttpFoundation.Session.SessionBagInterface;
+
+/**
+ * Attributes store.
+ *
+ * @memberOf Jymfony.Component.HttpFoundation.Session
+ */
+class AttributeBagInterface extends SessionBagInterface.definition {
+    /**
+     * Checks if an attribute is defined.
+     *
+     * @param {string} name The attribute name
+     *
+     * @returns {boolean} true if the attribute is defined, false otherwise
+     */
+    has(name) { }
+
+    /**
+     * Returns an attribute.
+     *
+     * @param {string} name The attribute name
+     * @param {*} [defaultValue] The default value if not found
+     *
+     * @returns {*}
+     */
+    get(name, defaultValue = undefined) { }
+
+    /**
+     * Sets an attribute.
+     *
+     * @param {string} name
+     * @param {*} value
+     */
+    set(name, value) { }
+
+    /**
+     * Returns attributes.
+     *
+     * @returns {Object.<string, *>} Attributes
+     */
+    all() { }
+
+    /**
+     * Sets attributes.
+     *
+     * @param {Object.<string, *>} attributes
+     */
+    replace(attributes) { }
+
+    /**
+     * Removes an attribute.
+     *
+     * @param {string} name
+     *
+     * @returns {*} The removed value or null when it does not exist
+     */
+    remove(name) { }
+}
+
+module.exports = AttributeBagInterface;
---
+++ src/Component/HttpFoundation/src/Session/SessionBagInterface.js
@@ -0,0 +1,36 @@
+/**
+ * Session Bag store.
+ *
+ * @memberOf Jymfony.Component.HttpFoundation.Session
+ */
+class SessionBagInterface {
+    /**
+     * Gets this bag's name.
+     *
+     * @returns {string}
+     */
+    get name() { }
+
+    /**
+     * Initializes the Bag.
+     *
+     * @param {Object.<string, *>} attributes
+     */
+    initialize(attributes) { }
+
+    /**
+     * Gets the storage key for this bag.
+     *
+     * @returns {string}
+     */
+    get storageKey() { }
+
+    /**
+     * Clears out data from bag.
+     *
+     * @returns {Object.<string, *>} Whatever data was contained
+     */
+    clear() { }
+}
+
+module.exports = getInterface(SessionBagInterface);
