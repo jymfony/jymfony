a884117d3b2a6c94479e8070dee192df53582e43
Alessandro Chitolina <alekitto@gmail.com>
2018-12-18 22:17:43 +0100

    [FrameworkBundle] add missing null on invalid reference for dev server

--- src/Bundle/FrameworkBundle/src/Resources/config/dev-server.js
+++ src/Bundle/FrameworkBundle/src/Resources/config/dev-server.js
@@ -1,10 +1,11 @@
 /** @global {Jymfony.Component.DependencyInjection.ContainerBuilder} container */
 
+const Container = Jymfony.Component.DependencyInjection.Container;
 const Reference = Jymfony.Component.DependencyInjection.Reference;
 
 container.register(Jymfony.Component.DevServer.DevServer, Jymfony.Component.DevServer.DevServer)
     .addArgument('%kernel.project_dir%')
-    .addArgument(new Reference('logger'))
+    .addArgument(new Reference('logger', Container.NULL_ON_INVALID_REFERENCE))
 ;
 
 container.register(Jymfony.Component.DevServer.Command.DevServerCommand, Jymfony.Component.DevServer.Command.DevServerCommand)
