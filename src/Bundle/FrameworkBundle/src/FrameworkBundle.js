5afe799e91a414804a6035623f9084f91c70fcdd
Alessandro Chitolina <alekitto@gmail.com>
2019-02-14 15:10:34 +0100

    [FrameworkBundle] routing is not a required dependency

--- src/Bundle/FrameworkBundle/src/FrameworkBundle.js
+++ src/Bundle/FrameworkBundle/src/FrameworkBundle.js
@@ -1,7 +1,6 @@
 const Bundle = Jymfony.Component.Kernel.Bundle;
 const PassConfig = Jymfony.Component.DependencyInjection.Compiler.PassConfig;
 const RegisterListenerPass = Jymfony.Component.EventDispatcher.DependencyInjection.Compiler.RegisterListenerPass;
-const RoutingResolverPass = Jymfony.Component.Routing.DependencyInjection.RoutingResolverPass;
 const LoggerChannelPass = Jymfony.Bundle.FrameworkBundle.DependencyInjection.Compiler.LoggerChannelPass;
 const TestServiceContainerRealRefPass = Jymfony.Bundle.FrameworkBundle.DependencyInjection.Compiler.TestServiceContainerRealRefPass;
 const TestServiceContainerWeakRefPass = Jymfony.Bundle.FrameworkBundle.DependencyInjection.Compiler.TestServiceContainerWeakRefPass;
@@ -31,13 +30,13 @@ class FrameworkBundle extends Bundle {
         container
             .addCompilerPass(new AddConsoleCommandPass())
             .addCompilerPass(new RegisterListenerPass(), PassConfig.TYPE_BEFORE_REMOVING)
-            .addCompilerPass(new RoutingResolverPass())
             .addCompilerPass(new LoggerChannelPass())
             .addCompilerPass(new TestServiceContainerWeakRefPass(), PassConfig.TYPE_BEFORE_REMOVING, -32)
             .addCompilerPass(new TestServiceContainerRealRefPass(), PassConfig.TYPE_AFTER_REMOVING)
             .addCompilerPass(new CachePoolPass(), PassConfig.TYPE_BEFORE_OPTIMIZATION, 32)
         ;
 
+        this._addCompilerPassIfExists(container, 'Jymfony.Component.Routing.DependencyInjection.RoutingResolverPass');
         this._addCompilerPassIfExists(container, 'Jymfony.Component.Mime.DependencyInjection.AddMimeTypeGuesserPass');
     }
 
