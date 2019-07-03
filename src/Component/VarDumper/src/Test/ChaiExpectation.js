6eb646bcdb8f96f2f51d0c321bc43407611f62dd
Alessandro Chitolina <alekitto@gmail.com>
2019-07-03 10:21:15 +0200

    [VarDumper] register chai expectations only if chai is installed

--- src/Component/VarDumper/src/Test/ChaiExpectation.js
+++ src/Component/VarDumper/src/Test/ChaiExpectation.js
@@ -1,6 +1,13 @@
+let chai;
+try {
+    chai = require('chai');
+} catch (e) {
+    chai = { };
+}
+
 const VarCloner = Jymfony.Component.VarDumper.Cloner.VarCloner;
 const CliDumper = Jymfony.Component.VarDumper.Dumper.CliDumper;
-const { Assertion, util } = require('chai');
+const { Assertion, util } = chai;
 const path = require('path');
 
 const getDump = (data, key = undefined, filter = 0) => {
@@ -26,40 +33,42 @@ const prepareExpectation = (expected, filter = 0) => {
     return __jymfony.trim(expected);
 };
 
-util.addChainableMethod(Assertion.prototype, 'dumpsAs', function(val, message = undefined) {
-    if (message) {
-        util.flag(this, 'message', message);
-    }
+if (!! util) {
+    util.addChainableMethod(Assertion.prototype, 'dumpsAs', function (val, message = undefined) {
+        if (message) {
+            util.flag(this, 'message', message);
+        }
 
-    const obj = getDump(util.flag(this, 'object'));
+        const obj = getDump(util.flag(this, 'object'));
 
-    this.assert(
-        prepareExpectation(val) === obj
-        , 'expected #{this} dumps the same as #{exp}'
-        , 'expected #{this} does not dump the same as #{exp}'
-        , val
-        , this._obj
-        , true
-    );
-});
-
-util.addChainableMethod(Assertion.prototype, 'dumpsAsFormat', function(str, message = undefined) {
-    str = prepareExpectation(str);
-    str = __jymfony.strtr(__jymfony.regex_quote(str), {
-        '/': '\\/',
-        '%%': '%',
-        '%e': '\\' + path.sep,
-        '%s': '[^\\r\\n]+',
-        '%S': '[^\\r\\n]*',
-        '%a': '.+',
-        '%A': '.*',
-        '%w': '\\s*',
-        '%i': '[+-]?\\d+',
-        '%d': '\\d+',
-        '%x': '[0-9a-fA-F]+',
-        '%f': '[+-]?\\.?\\d+\\.?\\d*(?:[Ee][+-]?\\d+)?',
-        '%c': '.',
+        this.assert(
+            prepareExpectation(val) === obj
+            , 'expected #{this} dumps the same as #{exp}'
+            , 'expected #{this} does not dump the same as #{exp}'
+            , val
+            , this._obj
+            , true
+        );
     });
 
-    new Assertion(getDump(this._obj)).to.match(new RegExp('^' + str + '$', 's'), message);
-});
+    util.addChainableMethod(Assertion.prototype, 'dumpsAsFormat', function (str, message = undefined) {
+        str = prepareExpectation(str);
+        str = __jymfony.strtr(__jymfony.regex_quote(str), {
+            '/': '\\/',
+            '%%': '%',
+            '%e': '\\' + path.sep,
+            '%s': '[^\\r\\n]+',
+            '%S': '[^\\r\\n]*',
+            '%a': '.+',
+            '%A': '.*',
+            '%w': '\\s*',
+            '%i': '[+-]?\\d+',
+            '%d': '\\d+',
+            '%x': '[0-9a-fA-F]+',
+            '%f': '[+-]?\\.?\\d+\\.?\\d*(?:[Ee][+-]?\\d+)?',
+            '%c': '.',
+        });
+
+        new Assertion(getDump(this._obj)).to.match(new RegExp('^' + str + '$', 's'), message);
+    });
+}
