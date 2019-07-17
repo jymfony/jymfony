bdc478f752e1de2cb80efc9e50c0fb4dd9ae5a8c
Alessandro Chitolina <alekitto@gmail.com>
2019-07-17 17:34:30 +0200

    [FrameworkBundle] framework bundle needs var dumper for tests

--- src/Bundle/FrameworkBundle/test/DependencyInjection/FrameworkExtensionTest.js
+++ src/Bundle/FrameworkBundle/test/DependencyInjection/FrameworkExtensionTest.js
@@ -52,4 +52,47 @@ describe('[FrameworkBundle] FrameworkExtension', function () {
         expect(this.container.hasDefinition('jymfony.logger')).to.be.true;
         expect(this.container.hasDefinition('jymfony.logger.handler.console')).to.be.true;
     });
+
+    it('should register http server configuration', () => {
+        this.extension.load([ {
+            http_server: {
+                enabled: true,
+            },
+        } ], this.container);
+
+        expect(this.container.hasDefinition(Jymfony.Component.HttpServer.HttpServer)).to.be.true;
+    });
+
+    it('should set http request timeout', () => {
+        this.extension.load([ {
+            http_server: {
+                enabled: true,
+                request_timeout: 30000,
+            },
+        } ], this.container);
+
+        expect(this.container.hasDefinition(Jymfony.Component.HttpServer.HttpServer)).to.be.true;
+        expect(
+            this.container.getDefinition(Jymfony.Component.HttpServer.HttpServer).getProperties()
+        ).to.be.deep.equal({ requestTimeoutMs: 30000 });
+    });
+
+    it('should set http key and certificate', () => {
+        this.extension.load([ {
+            http_server: {
+                enabled: true,
+                key: '/path/to/file.key',
+                certificate: '/path/to/certificate.pem',
+            },
+        } ], this.container);
+
+        expect(this.container.hasDefinition(Jymfony.Component.HttpServer.HttpServer)).to.be.true;
+
+        const def = this.container.getDefinition(Jymfony.Component.HttpServer.HttpServer);
+        expect(def.getClass()).to.be.equal('Jymfony.Component.HttpServer.Http2.HttpServer');
+        expect(def.getArgument(2)).to.be.deep.equal({
+            key: '/path/to/file.key',
+            cert: '/path/to/certificate.pem',
+        });
+    });
 });
