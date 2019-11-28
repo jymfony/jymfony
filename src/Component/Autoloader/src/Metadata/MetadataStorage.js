028a3732ad0a8944559aed8628168ef9ef4cb24e
Alessandro Chitolina <alekitto@gmail.com>
2019-11-28 17:30:38 +0100

    [Autoloader] allow metadata to be added multiple times

--- src/Component/Autoloader/src/Metadata/MetadataStorage.js
+++ src/Component/Autoloader/src/Metadata/MetadataStorage.js
@@ -4,30 +4,64 @@
 const storage = new WeakMap();
 const classSymbol = Symbol('class');
 
+/**
+ * Gets (or creates) the storage map for the given target/prop.
+ *
+ * @param {Function} target
+ * @param {null|string|symbol} prop
+ * @param {boolean} [create = false]
+ *
+ * @returns {Map<*, *>}
+ */
+const getStorage = (target, prop, create = false) => {
+    if (! storage.has(target)) {
+        storage.set(target, {});
+    }
+
+    if (! prop) {
+        prop = classSymbol;
+    }
+
+    const objStorage = storage.get(target);
+    if (undefined === objStorage[prop] && create) {
+        objStorage[prop] = new Map();
+    }
+
+    return objStorage[prop];
+};
+
 class MetadataStorage {
+    /**
+     * Adds a metadata value for the given key.
+     * This allows to use the same annotation multiple time on
+     * the same target.
+     *
+     * @param {*} key
+     * @param {*} value
+     * @param {Function} target
+     * @param {null|string|symbol} [prop]
+     */
+    static addMetadata(key, value, target, prop = null) {
+        const storage = getStorage(target, prop, true);
+        const currentValue = storage.get(key);
+        if (undefined === currentValue) {
+            storage.set(key, [ value ]);
+        } else {
+            storage.set(key, currentValue.push(value));
+        }
+    }
+
     /**
      * Defines a metadata.
      *
      * @param {*} key
      * @param {*} value
      * @param {Function} target
-     * @param {null|string|symbol} prop
+     * @param {null|string|symbol} [prop]
      */
-    static defineMetadata(key, value, target, prop) {
-        if (! storage.has(target)) {
-            storage.set(target, {});
-        }
-
-        if (! prop) {
-            prop = classSymbol;
-        }
-
-        const objStorage = storage.get(target);
-        if (undefined === objStorage[prop]) {
-            objStorage[prop] = new Map();
-        }
-
-        objStorage[prop].set(key, value);
+    static defineMetadata(key, value, target, prop = null) {
+        const storage = getStorage(target, prop, true);
+        storage.set(key, [ value ]);
     }
 
     /**
@@ -37,20 +71,14 @@ class MetadataStorage {
      * @param {null|string|symbol} prop
      */
     static getMetadata(target, prop) {
-        if (! storage.has(target)) {
+        const storage = getStorage(target, prop);
+        if (! storage) {
             return [];
         }
 
-        if (! prop) {
-            prop = classSymbol;
-        }
-
-        const objStorage = storage.get(target);
-        if (undefined === objStorage[prop]) {
-            return [];
-        }
-
-        return [ ...objStorage[prop].entries() ];
+        return [ ...storage.entries() ].map(
+            ([ key, value ]) => 1 === value.length ? [ key, value[0] ] : [ key, value ]
+        );
     }
 }
 
