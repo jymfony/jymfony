c64bfea31106654802773b012410b45bda60cfa2
Alessandro Chitolina <alekitto@gmail.com>
2019-12-29 17:02:17 +0100

    [VarDumper] add casters for data structure objects

---
+++ src/Component/VarDumper/src/Caster/DataStructureCaster.js
@@ -0,0 +1,40 @@
+const Caster = Jymfony.Component.VarDumper.Caster.Caster;
+
+/**
+ * @memberOf Jymfony.Component.VarDumper.Caster
+ */
+export default class DataStructureCaster {
+    /**
+     * Casts an HashTable object.
+     *
+     * @param {HashTable} hashTable
+     * @param {Object} a
+     * @param {Jymfony.Component.VarDumper.Cloner.Stub} stub
+     *
+     * @returns {Object}
+     */
+    static castHashTable(hashTable, a, stub) {
+        stub.class_ += ':' + hashTable.length;
+
+        return {
+            [Caster.PREFIX_VIRTUAL + 'content']: hashTable.toObject(),
+        };
+    }
+
+    /**
+     * Casts a data structure object.
+     *
+     * @param {PriorityQueue|LinkedList|BTree} object
+     * @param {Object} a
+     * @param {Jymfony.Component.VarDumper.Cloner.Stub} stub
+     *
+     * @returns {Object}
+     */
+    static castDataStructure(object, a, stub) {
+        stub.class_ += ':' + object.length;
+
+        return {
+            [Caster.PREFIX_VIRTUAL + 'content']: object.toArray(),
+        };
+    }
+}
--- src/DataStructure/types/globals.d.ts
+++ src/DataStructure/types/globals.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="BTree.d.ts" />
 /// <reference path="HashTable.d.ts" />
 /// <reference path="PriorityQueue.d.ts" />
+/// <reference path="LinkedList.d.ts" />
