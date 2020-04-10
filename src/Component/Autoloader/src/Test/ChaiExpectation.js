eb4445288abf5d2a7070f2cd15ab3870521673f7
Alessandro Chitolina <alekitto@gmail.com>
2020-04-10 17:00:22 +0200

    [Autoloader] fix class names in chai tests

---
+++ src/Component/Autoloader/src/Test/ChaiExpectation.js
@@ -0,0 +1,37 @@
+let chai;
+try {
+    chai = require('chai');
+} catch (e) {
+    chai = { };
+}
+
+const { util } = chai;
+
+if (!! util) {
+    const getName = util.getName;
+    util.getName = (func) => {
+        if (ReflectionClass.exists(func)) {
+            return ReflectionClass.getClassName(func);
+        }
+
+        return getName(func);
+    };
+
+    const compatibleConstructor = util.checkError.compatibleConstructor;
+    util.checkError.compatibleConstructor = (error, errorLike) => {
+        if ('function' === typeof errorLike && ReflectionClass.exists(errorLike)) {
+            errorLike = (new ReflectionClass(errorLike)).getConstructor();
+        }
+
+        return compatibleConstructor(error, errorLike);
+    };
+
+    const getConstructorName = util.checkError.getConstructorName;
+    util.checkError.getConstructorName = (errorLike) => {
+        if ('function' === typeof errorLike && ReflectionClass.exists(errorLike)) {
+            return ReflectionClass.getClassName(errorLike);
+        }
+
+        return getConstructorName(errorLike);
+    };
+}
