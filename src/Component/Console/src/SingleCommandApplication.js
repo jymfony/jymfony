50d5b35784f83e3c9a70c5bb33353c92f237c4bb
Alessandro Chitolina <alekitto@gmail.com>
2020-05-02 16:21:02 +0200

    [Console] add single command application helper

---
+++ src/Component/Console/src/SingleCommandApplication.js
@@ -0,0 +1,57 @@
+const Application = Jymfony.Component.Console.Application;
+const Command = Jymfony.Component.Console.Command.Command;
+
+/**
+ * @memberOf Jymfony.Component.Console
+ */
+export default class SingleCommandApplication extends Command {
+    __construct(name = process.argv[1]) {
+        super.__construct(name);
+
+        /**
+         * @type {string}
+         *
+         * @private
+         */
+        this._version = 'UNKNOWN';
+
+        /**
+         * @type {boolean}
+         *
+         * @private
+         */
+        this._running = false;
+    }
+
+    /**
+     * Sets the application version.
+     *
+     * @param {string} version
+     */
+    set version(version) {
+        this._version = version;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    async run(input = undefined, output = undefined) {
+        if (this._running) {
+            return super.run(input, output);
+        }
+
+        // We use the command name as the application name
+        const application = new Application(this.name || 'UNKNOWN', this._version);
+
+        // Fix the usage of the command displayed with "--help"
+        application.add(this);
+        application.defaultCommand = this.name;
+
+        this._running = true;
+        try {
+            return application.run(input, output);
+        } finally {
+            this._running = false;
+        }
+    }
+}
