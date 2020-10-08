32e3c3678f07ae9d9bf751012a616b6051e40d6e
Alessandro Chitolina <alekitto@gmail.com>
2020-10-08 11:57:16 +0200

    fix standalone test run

--- src/Exceptions/tests.js
+++ src/Exceptions/tests.js
@@ -1,8 +1,13 @@
 require('@jymfony/autoloader');
-require('./index.js');
 
 const Debug = Jymfony.Component.Debug.Debug;
 const Runner = Jymfony.Component.Testing.Framework.Runner;
 Debug.enable();
 
+const [ , , ...argv ] = [ ...process.argv ];
+if (0 === argv.length) {
+    argv.push('test/**/*.js');
+}
+
+process.argv = argv;
 new Runner().run();
