ceb7d60394e5bf414f00606073dbf1768b28f677
Alessandro Chitolina <alekitto@gmail.com>
2020-10-16 12:14:26 +0200

    [util] strtr now accepts from/to strings

--- src/util/test/String/StrtrTest.js
+++ src/util/test/String/StrtrTest.js
@@ -1,22 +1,29 @@
-require('../../lib/String/strtr');
-const { expect } = require('chai');
+import '../../lib/String/strtr';
+import { expect } from 'chai';
 
-describe('Strtr', function () {
-    it('should work correctly', () => {
+const TestCase = Jymfony.Component.Testing.Framework.TestCase;
+
+export default class StrtrTest extends TestCase {
+    testShouldWorkCorrectlyWithObjects() {
         const trans = {'hello': 'hi', 'hi': 'hello', 'a': 'A', 'world': 'planet'};
         expect(__jymfony.strtr('# hi all, I said hello world! #', trans))
             .to.be.equal('# hello All, I sAid hi planet! #');
-    });
+    }
 
-    it('should work correctly with regex special chars', () => {
+    testShouldWorkCorrectlyWithRegexSpecialChars() {
         const trans = {'hello': 'hi', '\\hi?': 'hello#', '(a': ')A', 'world': 'planet'};
         expect(__jymfony.strtr('# \\hi? (all), I said hello world! #', trans))
             .to.be.equal('# hello# )All), I said hi planet! #');
-    });
+    }
 
-    it('should work correctly with no replace pairs', () => {
+    testShouldWorkCorrectlyWithNoReplacePairs() {
         const trans = {};
         expect(__jymfony.strtr('# \\hi? (all), I said hello world! #', trans))
             .to.be.equal('# \\hi? (all), I said hello world! #');
-    });
-});
+    }
+
+    testShouldWorkCorrectlyWithFromToStrings() {
+        expect(__jymfony.strtr('abcdefg', 'acd', 'ACD'))
+            .to.be.equal('AbCDefg');
+    }
+}
