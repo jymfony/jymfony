f3482326f382c3dc9841cf638c7301a2eb7a677d
Alessandro Chitolina <alekitto@gmail.com>
2022-12-28 15:34:33 +0100

    [FrameworkBundle] fix(services): add alias for ContainerInterface

--- src/Bundle/FrameworkBundle/src/Resources/config/services.js
+++ src/Bundle/FrameworkBundle/src/Resources/config/services.js
@@ -4,6 +4,8 @@ const Alias = Jymfony.Component.DependencyInjection.Alias;
 const TaggedIteratorArgument = Jymfony.Component.DependencyInjection.Argument.TaggedIteratorArgument;
 const Reference = Jymfony.Component.DependencyInjection.Reference;
 
+container.setAlias(Jymfony.Component.DependencyInjection.ContainerInterface, 'service_container');
+
 container.register('parameter_bag', Jymfony.Component.DependencyInjection.ParameterBag.ContainerBag)
     .addArgument(new Reference('service_container'))
 ;
