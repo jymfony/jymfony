cd3589ccbc0f80de776a38c8f11753f87a64bae3
Alessandro Chitolina <alekitto@gmail.com>
2023-03-27 16:52:04 +0200

    chore: minimum node.js version required is 12

--- src/Component/Validator/test/ValidationTest.js
+++ src/Component/Validator/test/ValidationTest.js
@@ -1,21 +1,17 @@
 const Email = Jymfony.Component.Validator.Constraints.Email;
+const TestCase = Jymfony.Component.Testing.Framework.TestCase;
 const ValidationFailedException = Jymfony.Component.Validator.Exception.ValidationFailedException;
 const Validation = Jymfony.Component.Validator.Validation;
-const { expect } = require('chai');
 
-describe('[Validator] Validation', function () {
-    it ('createCallable should work', async () => {
+export default class ValidationTest extends TestCase {
+    async testCreateCallableShouldWork() {
         const validator = Validation.createCallable(new Email());
-        expect(await validator('text@example.com')).to.be.equal('text@example.com');
-    });
+        __self.assertEquals('test@example.org', await validator('test@example.org'));
+    }
 
-    it ('createCallable should throw on invalid value', async () => {
+    async testCreateCallableShouldThrowOnInvalidValue() {
         const validator = Validation.createCallable(new Email());
-        try {
-            await validator('test');
-            throw new Error('FAIL');
-        } catch (e) {
-            expect(e).to.be.instanceOf(ValidationFailedException);
-        }
-    });
-});
+        this.expectException(ValidationFailedException);
+        await validator('test');
+    }
+}
