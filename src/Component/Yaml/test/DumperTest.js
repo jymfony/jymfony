323037a2a8ae638a1f261c617d22332d0787609b
Alessandro Chitolina <alekitto@gmail.com>
2023-04-11 14:55:23 +0200

    [Yaml] chore: remove chai dependency

--- src/Component/Yaml/test/DumperTest.js
+++ src/Component/Yaml/test/DumperTest.js
@@ -1,66 +1,25 @@
 const Dumper = Jymfony.Component.Yaml.Dumper;
 const Parser = Jymfony.Component.Yaml.Parser;
+const TestCase = Jymfony.Component.Testing.Framework.TestCase;
 const Yaml = Jymfony.Component.Yaml.Yaml;
-const { expect } = require('chai');
 const { readFileSync } = require('fs');
 
 const fixturesDir = __dirname + '/../fixtures/';
 
-function loadTestsFromFixtureFiles(testsFile) {
-    const parser = new Parser();
+export default class DumperTest extends TestCase {
+    _dumper;
+    _parser;
 
-    const tests = [];
-    const files = parser.parseFile(fixturesDir + testsFile);
-    for (const file of files) {
-        const yamls = readFileSync(fixturesDir + file + '.yml').toString('utf-8');
-
-        // Split YAMLs documents
-        for (const yaml of yamls.split(/^--- %YAML:1\.0|^---/mg)) {
-            if (! yaml) {
-                continue;
-            }
-
-            const test = parser.parse(yaml);
-            if (!! test.dump_skip) {
-                continue;
-            }
-
-            if (!! test.todo) {
-                tests.push([ null, null, null ]);
-            } else {
-                let expected;
-                eval('expected = ' + __jymfony.trim(test['js']) + ';');
-
-                tests.push([ expected, test['test'] ]);
-            }
-        }
+    get testCaseName() {
+        return '[Yaml] ' + super.testCaseName;
     }
 
-    return tests;
-}
-
-const arr = {
-    '': 'bar',
-    'foo': '#bar',
-    'foo\'bar': {},
-    'bar': [ 1, 'foo' ],
-    'foobar': {
-        'foo': 'bar',
-        'bar': [ 1, 'foo' ],
-        'foobar': {
-            'foo': 'bar',
-            'bar': [ 1, 'foo' ],
-        },
-    },
-};
-
-describe('[Yaml] Dumper', function () {
-    beforeEach(() => {
+    beforeEach() {
         this._dumper = new Dumper();
         this._parser = new Parser();
-    });
+    }
 
-    it ('should respect indentation in constructor', () => {
+    testShouldRespectIndentationInConstructor() {
         const dumper = new Dumper(7);
         const expected = `'': bar
 foo: '#bar'
@@ -80,41 +39,85 @@ foobar:
                      - foo
 `;
 
-        expect(dumper.dump(arr, 4, 0)).to.be.eq(expected);
-    });
-
-    for (const [ index, [ expected, comment ] ] of __jymfony.getEntries(loadTestsFromFixtureFiles('index.yml'))) {
-        it ('should respect specification #' + index, null !== expected ? () => {
-            const dumped = this._dumper.dump(expected, 10);
-            expect(this._parser.parse(dumped, Yaml.PARSE_CUSTOM_TAGS)).to.be.deep.eq(expected, comment);
-        } : undefined);
+        __self.assertEquals(expected, dumper.dump({
+            '': 'bar',
+            'foo': '#bar',
+            'foo\'bar': {},
+            'bar': [ 1, 'foo' ],
+            'foobar': {
+                'foo': 'bar',
+                'bar': [ 1, 'foo' ],
+                'foobar': {
+                    'foo': 'bar',
+                    'bar': [ 1, 'foo' ],
+                },
+            },
+        }, 4, 0));
     }
 
-    const escapeSequences = {
-        'empty string': [ '', '\'\'' ],
-        'null': [ '\x00', '"\\0"' ],
-        'bell': [ '\x07', '"\\a"' ],
-        'backspace': [ '\x08', '"\\b"' ],
-        'horizontal-tab': [ '\t', '"\\t"' ],
-        'line-feed': [ '\n', '"\\n"' ],
-        'vertical-tab': [ '\v', '"\\v"' ],
-        'form-feed': [ '\x0C', '"\\f"' ],
-        'carriage-return': [ '\r', '"\\r"' ],
-        'escape': [ '\x1B', '"\\e"' ],
-        'space': [ ' ', '\' \'' ],
-        'double-quote': [ '"', '\'"\'' ],
-        'slash': [ '/', '/' ],
-        'backslash': [ '\\', '\\' ],
-        'next-line': [ '\xC2\x85', '"\\N"' ],
-        'non-breaking-space': [ '\xc2\xa0', '"\\_"' ],
-        'line-separator': [ '\xE2\x80\xA8', '"\\L"' ],
-        'paragraph-separator': [ '\xE2\x80\xA9', '"\\P"' ],
-        'colon': [ ':', '\':\'' ],
-    };
+    @dataProvider('provideSpecificationsTests')
+    testShouldRespectSpecification(expected, comment) {
+        if (null === expected) {
+            __self.markTestSkipped();
+        }
 
-    for (const [ comment, [ input, expected ] ] of __jymfony.getEntries(escapeSequences)) {
-        it ('should escape ' + comment, () => {
-            expect(this._dumper.dump(input)).to.be.eq(expected);
-        });
+        const dumped = this._dumper.dump(expected, 10);
+        __self.assertEquals(expected, this._parser.parse(dumped, Yaml.PARSE_CUSTOM_TAGS), comment);
     }
-});
+
+    * provideSpecificationsTests() {
+        const parser = new Parser();
+        const files = parser.parseFile(fixturesDir + 'index.yml');
+        for (const file of files) {
+            const yamls = readFileSync(fixturesDir + file + '.yml').toString('utf-8');
+
+            // Split YAMLs documents
+            for (const yaml of yamls.split(/^--- %YAML:1\.0|^---/mg)) {
+                if (! yaml) {
+                    continue;
+                }
+
+                const test = parser.parse(yaml);
+                if (!! test.dump_skip) {
+                    continue;
+                }
+
+                if (!! test.todo) {
+                    yield [ null, null ];
+                } else {
+                    let expected;
+                    eval('expected = ' + __jymfony.trim(test['js']) + ';');
+
+                    yield [ expected, test.test ];
+                }
+            }
+        }
+    }
+
+    @dataProvider('provideEscapeTests')
+    testShouldEscapeCharacters(comment, input, expected) {
+        __self.assertEquals(expected, this._dumper.dump(input), comment);
+    }
+
+    * provideEscapeTests() {
+        yield [ 'empty string', '', '\'\'' ];
+        yield [ 'null', '\x00', '"\\0"' ];
+        yield [ 'bell', '\x07', '"\\a"' ];
+        yield [ 'backspace', '\x08', '"\\b"' ];
+        yield [ 'horizontal-tab', '\t', '"\\t"' ];
+        yield [ 'line-feed', '\n', '"\\n"' ];
+        yield [ 'vertical-tab', '\v', '"\\v"' ];
+        yield [ 'form-feed', '\x0C', '"\\f"' ];
+        yield [ 'carriage-return', '\r', '"\\r"' ];
+        yield [ 'escape', '\x1B', '"\\e"' ];
+        yield [ 'space', ' ', '\' \'' ];
+        yield [ 'double-quote', '"', '\'"\'' ];
+        yield [ 'slash', '/', '/' ];
+        yield [ 'backslash', '\\', '\\' ];
+        yield [ 'next-line', '\xC2\x85', '"\\N"' ];
+        yield [ 'non-breaking-space', '\xc2\xa0', '"\\_"' ];
+        yield [ 'line-separator', '\xE2\x80\xA8', '"\\L"' ];
+        yield [ 'paragraph-separator', '\xE2\x80\xA9', '"\\P"' ];
+        yield [ 'colon', ':', '\':\'' ];
+    }
+}
--- src/Component/Yaml/test/ParserTest.js
+++ src/Component/Yaml/test/ParserTest.js
@@ -1,86 +1,91 @@
 const Parser = Jymfony.Component.Yaml.Parser;
+const TestCase = Jymfony.Component.Testing.Framework.TestCase;
 const Yaml = Jymfony.Component.Yaml.Yaml;
-const { expect } = require('chai');
 const { readFileSync, chmodSync } = require('fs');
 
 const fixturesDir = __dirname + '/../fixtures/';
 
-function loadTestsFromFixtureFiles(testsFile) {
-    const parser = new Parser();
+export default class ParserTest extends TestCase {
+    _parser;
 
-    const tests = [];
-    const files = parser.parseFile(fixturesDir + testsFile);
-    for (const file of files) {
-        const yamls = readFileSync(fixturesDir + file + '.yml').toString('utf-8');
+    beforeEach() {
+        this._parser = new Parser();
+    }
 
-        // Split YAMLs documents
-        for (const yaml of yamls.split(/^--- %YAML:1\.0|^---/mg)) {
-            if (! yaml) {
-                continue;
-            }
+    afterEach() {
+        chmodSync(fixturesDir + '/not_readable.yml', 0o644);
+    }
 
-            const test = parser.parse(yaml);
-            if (!! test.todo) {
-                tests.push([ null, null, null ]);
-            } else {
-                let expected;
-                eval('expected = ' + __jymfony.trim(test['js']) + ';');
+    @dataProvider('provideDataFromFixturesFile')
+    testShouldRespectSpecification(expected, yaml, comment) {
+        if (null === expected) {
+            __self.markTestSkipped();
+        }
 
-                tests.push([ expected, test['yaml'], test['test'] ]);
+        __self.assertEquals(expected, this._parser.parse(yaml, Yaml.PARSE_CUSTOM_TAGS), comment);
+    }
+
+    provideDataFromFixturesFile() {
+        const parser = new Parser();
+
+        const tests = [];
+        const files = parser.parseFile(fixturesDir + 'index.yml');
+        for (const file of files) {
+            const yamls = readFileSync(fixturesDir + file + '.yml').toString('utf-8');
+
+            // Split YAMLs documents
+            for (const yaml of yamls.split(/^--- %YAML:1\.0|^---/mg)) {
+                if (! yaml) {
+                    continue;
+                }
+
+                const test = parser.parse(yaml);
+                if (!! test.todo) {
+                    tests.push([ null, null, null ]);
+                } else {
+                    let expected;
+                    eval('expected = ' + __jymfony.trim(test['js']) + ';');
+
+                    tests.push([ expected, test['yaml'], test['test'] ]);
+                }
             }
         }
+
+        return tests;
     }
 
-    return tests;
-}
-
-describe('[Yaml] Parser', function () {
-    beforeEach(() => {
-        this._parser = new Parser();
-    });
-
-    afterEach(() => {
-        chmodSync(fixturesDir + '/not_readable.yml', 0o644);
-    });
-
-    for (const [ index, [ expected, yaml, comment ] ] of __jymfony.getEntries(loadTestsFromFixtureFiles('index.yml'))) {
-        it ('should respect specification #' + index, null !== expected ? () => {
-            expect(this._parser.parse(yaml, Yaml.PARSE_CUSTOM_TAGS)).to.be.deep.equal(expected, comment);
-        } : undefined);
-    }
-
-    it ('should throw eval ref exception', () => {
+    testShouldThrowEvalRefException() {
         const yaml = 'foo: { &foo { a: Steve, <<: *foo} }';
 
-        expect(() => this._parser.parse(yaml)).to.throw(/Reference "foo" does not exist/);
-    });
+        this.expectExceptionMessageRegex(/Reference "foo" does not exist/);
+        this._parser.parse(yaml);
+    }
 
-    const circularReferenceTests = [
-        [ 'sequence', `
+    * provideCircularReferenceTests() {
+        yield [ `
 foo:
     - &foo
       - &bar
         bar: foobar
         baz: *foo
-` ],
-        [ 'mapping', `
+` ];
+        yield [ `
 foo: &foo
     bar: &bar
         foobar: baz
         baz: *foo
-` ],
-        [ 'mapping with merge key', `
+` ];
+        yield [ `
 foo: &foo
     bar: &bar
         foobar: baz
         <<: *foo
-` ],
-    ];
-
-    for (const [ name, yaml ] of circularReferenceTests) {
-        it ('should detect circular reference: ' + name, () => {
-            expect(() => this._parser.parse(yaml, Yaml.PARSE_CUSTOM_TAGS))
-                .to.throw(/Circular reference \[foo, bar, foo\] detected/);
-        });
+` ];
     }
-});
+
+    @dataProvider('provideCircularReferenceTests')
+    testShouldDetectCircularReference(yaml) {
+        this.expectExceptionMessageRegex(/Circular reference \[foo, bar, foo\] detected/);
+        this._parser.parse(yaml, Yaml.PARSE_CUSTOM_TAGS);
+    }
+}
