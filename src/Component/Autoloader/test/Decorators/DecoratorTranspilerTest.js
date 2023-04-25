6e09bb374e6d8701131b3477bd6bc219430de717
Alessandro Chitolina <alekitto@gmail.com>
2023-04-25 10:19:04 +0200

    wip

--- src/Component/Autoloader/test/Decorators/DecoratorTranspilerTest.js
+++ src/Component/Autoloader/test/Decorators/DecoratorTranspilerTest.js
@@ -1,33 +1,32 @@
-const { expect } = require('chai');
-const { join } = require('path');
+import { join } from 'path';
 
-/*
- * We are testing autoloader component here
- * Cannot use the autoloader itself to load classes! :)
- */
-require('../../src/Autoloader');
-const Namespace = require('../../src/Namespace');
+const Namespace = Jymfony.Component.Autoloader.Namespace;
+const TestCase = Jymfony.Component.Testing.Framework.TestCase;
 const fixturesDir = join(__dirname, '..', '..', 'fixtures');
 
-describe('[Autoloader] Annotations transpiler', () => {
-    afterEach(() => {
-        delete global.Foo;
-    });
+export default class DecoratorTranspilerTest extends TestCase {
+    get testCaseName() {
+        return '[Autoloader] ' + super.testCaseName;
+    }
 
-    it('should transpile annotations', __jymfony.Platform.hasPublicFieldSupport() ? () => {
+    afterEach() {
+        delete global.Foo;
+    }
+
+    testShouldTranspileAnnotations() {
         global.Foo = new Namespace(__jymfony.autoload, 'Foo', fixturesDir, __jymfony.autoload.classLoader._internalRequire);
         const a = new Foo.Annotated();
         const r = new ReflectionClass(a);
 
         const annotation = new ReflectionClass(Foo.Decorators.TestAnnotation).getConstructor();
 
-        expect(r.metadata).to.have.lengthOf(1);
-        expect(r.metadata[0][0]).to.be.eq(annotation);
-        expect(r.metadata[0][1][1]).to.be.instanceOf(Foo.Decorators.TestAnnotation);
-        expect(r.metadata[0][1][1]._value).to.be.deep.equal({ value: 24 });
+        __self.assertCount(1, r.metadata);
+        __self.assertEquals(annotation, r.metadata[0][0]);
+        __self.assertInstanceOf(Foo.Decorators.TestAnnotation, r.metadata[0][1][1]);
+        __self.assertEquals({ value: 24 }, r.metadata[0][1][1]._value);
 
-        expect(r.getField('_value').metadata).to.be.deep.equal([ [ annotation, { _value: { prop: 'test' } } ] ]);
-        expect(r.getMethod('getValue').metadata).to.be.deep.equal([ [ annotation, { _value: undefined } ] ]);
-        expect(r.getReadableProperty('value').metadata).to.be.deep.equal([ [ annotation, { _value: null } ] ]);
-    } : undefined);
-});
+        __self.assertEquals([ [ annotation, new Foo.Decorators.TestAnnotation({ prop: 'test' }) ] ], r.getField('_value').metadata);
+        __self.assertEquals([ [ annotation, new Foo.Decorators.TestAnnotation(undefined) ] ], r.getMethod('getValue').metadata);
+        __self.assertEquals([ [ annotation, new Foo.Decorators.TestAnnotation(null) ] ], r.getReadableProperty('value').metadata);
+    }
+}
