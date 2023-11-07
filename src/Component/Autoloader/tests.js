8c57b4a5ba2bada9450df880df1bd57dd14071c8
Alessandro Chitolina <alekitto@gmail.com>
2023-11-07 17:51:52 +0100

    [Autoloader] fix(tests): run tests through testing component

--- src/Component/Autoloader/tests.js
+++ src/Component/Autoloader/tests.js
@@ -1,2 +1,14 @@
 require('./index');
-require('mocha/bin/_mocha');
+
+const Debug = Jymfony.Component.Debug.Debug;
+const Runner = Jymfony.Component.Testing.Framework.Runner;
+Debug.enable();
+
+const [ , , ...argv ] = [ ...process.argv ];
+if (0 === argv.length) {
+    argv.push('test/**/*.js');
+}
+
+process.argv = argv;
+new Runner().run();
+
